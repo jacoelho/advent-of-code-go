528f8f10bd3f8d862200c36e115e917efbbfcc8e
Jose Coelho <4838079+jacoelho@users.noreply.github.com>
2024-12-15 16:52:43 +0000

    feat: 2024-15

---
+++ internal/xmaps/xmaps.go
@@ -0,0 +1,31 @@
+package xmaps
+
+type Pair[V1, V2 any] struct {
+	K V1
+	V V2
+}
+
+func Find[M ~map[K]V, K comparable, V any](m M, predicate func(k K, v V) bool) (Pair[K, V], bool) {
+	for k, v := range m {
+		if predicate(k, v) {
+			return Pair[K, V]{K: k, V: v}, true
+		}
+	}
+	var empty Pair[K, V]
+	return empty, false
+}
+
+func Filter[M ~map[K]V, K comparable, V any](m M, predicate func(k K, v V) bool) []Pair[K, V] {
+	if len(m) == 0 {
+		return nil
+	}
+
+	var result []Pair[K, V]
+	for k, v := range m {
+		if predicate(k, v) {
+			result = append(result, Pair[K, V]{K: k, V: v})
+		}
+	}
+
+	return result
+}
