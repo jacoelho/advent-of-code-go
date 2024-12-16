cd0b4c0a45ba14a8f8044e895e1856db1d56f339
Jose Coelho <4838079+jacoelho@users.noreply.github.com>
2024-12-16 19:06:14 +0000

    feat: 2024-16

---
+++ internal/aoc2024/day16.go
@@ -0,0 +1,112 @@
+package aoc2024
+
+import (
+	"io"
+	"slices"
+	"strconv"
+
+	"github.com/jacoelho/advent-of-code-go/internal/collections"
+
+	"github.com/jacoelho/advent-of-code-go/internal/aoc"
+	"github.com/jacoelho/advent-of-code-go/internal/search"
+
+	"github.com/jacoelho/advent-of-code-go/internal/xmaps"
+
+	"github.com/jacoelho/advent-of-code-go/internal/grid"
+	"github.com/jacoelho/advent-of-code-go/internal/scanner"
+)
+
+func parseReindeerMaze(r io.Reader) (grid.Grid2D[int, rune], error) {
+	s := scanner.NewScanner(r, func(b []byte) ([]rune, error) {
+		res := make([]rune, len(b))
+		for i, v := range b {
+			res[i] = rune(v)
+		}
+		return res, nil
+	})
+	return grid.NewGrid2D[int, rune](slices.Collect(s.Values())), s.Err()
+}
+
+func mazeStartPosition(g grid.Grid2D[int, rune]) grid.Position2D[int] {
+	element, ok := xmaps.Find(g, func(p grid.Position2D[int], v rune) bool { return v == 'S' })
+	if !ok {
+		panic("not found")
+	}
+	return element.K
+}
+
+type day16Pair struct {
+	position  grid.Position2D[int]
+	direction grid.Position2D[int]
+}
+
+func day16Neighbours(maze grid.Grid2D[int, rune]) func(p day16Pair) []day16Pair {
+	return func(p day16Pair) []day16Pair {
+		forward := day16Pair{
+			position:  p.position.Add(p.direction),
+			direction: p.direction,
+		}
+		clockwise := day16Pair{
+			position:  p.position,
+			direction: p.direction.TurnRight(),
+		}
+		counter := day16Pair{
+			position:  p.position,
+			direction: p.direction.TurnLeft(),
+		}
+		var result []day16Pair
+		for _, candidate := range []day16Pair{forward, clockwise, counter} {
+			if maze[candidate.position] == '.' || maze[candidate.position] == 'E' || maze[candidate.position] == 'S' {
+				result = append(result, candidate)
+			}
+		}
+		return result
+	}
+}
+
+func day16Heuristic(maze grid.Grid2D[int, rune]) func(pair day16Pair) int {
+	return func(pair day16Pair) int {
+		if maze[pair.position] == 'E' {
+			return 0
+		}
+		return 1
+	}
+}
+
+func day16Cost(p1, p2 day16Pair) int {
+	if p1.position.Add(p1.direction) == p2.position {
+		return 1
+	}
+	return 1000
+}
+
+func day16p01(r io.Reader) (string, error) {
+	maze := aoc.Must(parseReindeerMaze(r))
+	start := day16Pair{
+		position:  mazeStartPosition(maze),
+		direction: grid.Position2D[int]{1, 0},
+	}
+
+	score, _, _ := search.AStar(start, day16Neighbours(maze), day16Heuristic(maze), day16Cost)
+
+	return strconv.Itoa(score), nil
+}
+
+func day16p02(r io.Reader) (string, error) {
+	maze := aoc.Must(parseReindeerMaze(r))
+	start := day16Pair{
+		position:  mazeStartPosition(maze),
+		direction: grid.Position2D[int]{1, 0},
+	}
+
+	_, paths, _ := search.AStarBag(start, day16Neighbours(maze), day16Heuristic(maze), day16Cost)
+
+	visited := collections.NewSet[grid.Position2D[int]]()
+	for _, path := range paths {
+		for _, element := range path {
+			visited.Add(element.position)
+		}
+	}
+
+	return strconv.Itoa(visited.Len()), nil
+}
--- internal/search/astar.go
+++ internal/search/astar.go
@@ -61,3 +61,79 @@ func AStar[T comparable](
 
 	return 0, nil, false
 }
+
+// AStarBag visits all paths with the lowest cost
+func AStarBag[T comparable](
+	start T,
+	neighbours func(T) []T,
+	heuristic func(T) int,
+	stepCost func(T, T) int,
+) (int, [][]T, bool) {
+	type minItem[T any] struct {
+		item     T
+		priority int
+	}
+
+	priorityQueue := collections.NewHeap[minItem[T]](func(m minItem[T], m2 minItem[T]) bool {
+		return m.priority < m2.priority
+	})
+
+	priorityQueue.Push(minItem[T]{
+		item:     start,
+		priority: heuristic(start),
+	})
+
+	paths := make(map[T][][]T)
+	paths[start] = [][]T{{start}}
+
+	pathCost := make(map[T]int)
+	pathCost[start] = 0
+
+	var goalPaths [][]T
+	lowestGoalCost := -1
+
+	for current := range priorityQueue.PopSeq() {
+		if heuristic(current.item) == 0 {
+			currentCost := pathCost[current.item]
+			if lowestGoalCost == -1 || currentCost == lowestGoalCost {
+				lowestGoalCost = currentCost
+				goalPaths = append(goalPaths, paths[current.item]...)
+			} else if currentCost > lowestGoalCost {
+				break
+			}
+			continue
+		}
+
+		for _, neighbor := range neighbours(current.item) {
+			newCost := pathCost[current.item] + stepCost(current.item, neighbor)
+			if oldCost, ok := pathCost[neighbor]; !ok || newCost < oldCost {
+				// Found a better path; update costs and paths
+				pathCost[neighbor] = newCost
+				priorityQueue.Push(minItem[T]{
+					item:     neighbor,
+					priority: newCost + heuristic(neighbor),
+				})
+
+				var newPaths [][]T
+				for _, path := range paths[current.item] {
+					newPath := append([]T{}, path...)
+					newPath = append(newPath, neighbor)
+					newPaths = append(newPaths, newPath)
+				}
+				paths[neighbor] = newPaths
+			} else if newCost == oldCost {
+				for _, path := range paths[current.item] {
+					newPath := append([]T{}, path...)
+					newPath = append(newPath, neighbor)
+					paths[neighbor] = append(paths[neighbor], newPath)
+				}
+			}
+		}
+	}
+
+	if len(goalPaths) > 0 {
+		return lowestGoalCost, goalPaths, true
+	}
+
+	return 0, nil, false
+}
